38eafc67500319af3ad2624a823b3cd454d9b36c
Dimitri Tarnavski <24656477+fayras@users.noreply.github.com>
2019-01-29 23:48:58 +0100

    deploy

---
+++ precache-manifest.debda3f40bc480860a519a2d97126e99.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = [
+  {
+    "revision": "f058a41b1dd10198c4ac",
+    "url": "/bug-busters/js/about.691d396e.js"
+  },
+  {
+    "revision": "98fa98c276f9778a6575",
+    "url": "/bug-busters/css/app.d2021db5.css"
+  },
+  {
+    "revision": "98fa98c276f9778a6575",
+    "url": "/bug-busters/js/app.cfa901c2.js"
+  },
+  {
+    "revision": "9e7ec560f5961c828aab",
+    "url": "/bug-busters/css/chunk-vendors.a66130bb.css"
+  },
+  {
+    "revision": "9e7ec560f5961c828aab",
+    "url": "/bug-busters/js/chunk-vendors.ddc0e814.js"
+  },
+  {
+    "revision": "07f7e69c57dab692d8d5",
+    "url": "/bug-busters/css/room.7a90a648.css"
+  },
+  {
+    "revision": "07f7e69c57dab692d8d5",
+    "url": "/bug-busters/js/room.82a362a7.js"
+  },
+  {
+    "revision": "91d69011d345f1c623456b968db471df",
+    "url": "/bug-busters/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bug-busters/robots.txt"
+  },
+  {
+    "revision": "0480a35aa9ed8678ed552683e58e34ce",
+    "url": "/bug-busters/three/examples/js/loaders/AssimpJSONLoader.js"
+  },
+  {
+    "revision": "9f6d9febf7eeb809ac2a54f78e627ae4",
+    "url": "/bug-busters/three/examples/js/loaders/BabylonLoader.js"
+  },
+  {
+    "revision": "58b4c6f025a9a06aa15398aaf7f167ec",
+    "url": "/bug-busters/three/examples/js/loaders/DDSLoader.js"
+  },
+  {
+    "revision": "b6cad92843b8429949e3cc2472a8be73",
+    "url": "/bug-busters/three/examples/js/loaders/EquirectangularToCubeGenerator.js"
+  },
+  {
+    "revision": "ad113395f878a0222ab3893297ee87d5",
+    "url": "/bug-busters/three/examples/js/loaders/GCodeLoader.js"
+  },
+  {
+    "revision": "36d56cfe829493f69367841cd8d87405",
+    "url": "/bug-busters/three/examples/js/loaders/HDRCubeTextureLoader.js"
+  },
+  {
+    "revision": "70f01c61652c0a65f3e5ca571be01263",
+    "url": "/bug-busters/three/examples/js/loaders/KMZLoader.js"
+  },
+  {
+    "revision": "79fd12167c117354939c1451e8680b00",
+    "url": "/bug-busters/three/examples/js/loaders/NodeMaterialLoader.js"
+  },
+  {
+    "revision": "e81e7cae9d9fdbc0404e0b916e6c7078",
+    "url": "/bug-busters/three/examples/js/loaders/KTXLoader.js"
+  },
+  {
+    "revision": "0362e2a0c0c98f808ee78fe286f7da63",
+    "url": "/bug-busters/three/examples/js/loaders/PCDLoader.js"
+  },
+  {
+    "revision": "93efc956d082b3ee3223c4d7567a5c2c",
+    "url": "/bug-busters/three/examples/js/loaders/PDBLoader.js"
+  },
+  {
+    "revision": "0c8771fd3319b2546a4d13b17cf54cfd",
+    "url": "/bug-busters/three/examples/js/loaders/PlayCanvasLoader.js"
+  },
+  {
+    "revision": "ac1d9ac9907f45eb6d7eb3f15b09696b",
+    "url": "/bug-busters/three/examples/js/loaders/PVRLoader.js"
+  },
+  {
+    "revision": "bb90459928227a99074b172f372fe933",
+    "url": "/bug-busters/three/examples/js/loaders/PRWMLoader.js"
+  },
+  {
+    "revision": "68f1e5c649670f85ce7d33582fbf08c8",
+    "url": "/bug-busters/three/examples/js/loaders/VRMLoader.js"
+  },
+  {
+    "revision": "42d5e372e1f9066313fd68e725bcba19",
+    "url": "/bug-busters/three/examples/js/loaders/TTFLoader.js"
+  },
+  {
+    "revision": "f28e464a15aa8ab8bb8056eebc4614e0",
+    "url": "/bug-busters/three/examples/js/postprocessing/EffectComposer.js"
+  },
+  {
+    "revision": "f118c91f2b55c04c056f8cb2bbf23317",
+    "url": "/bug-busters/three/examples/js/postprocessing/RenderPass.js"
+  },
+  {
+    "revision": "0ab701d28355bf4fd808fb83338d99db",
+    "url": "/bug-busters/three/examples/js/postprocessing/ShaderPass.js"
+  },
+  {
+    "revision": "52c4f437fc40e671bfd8ca8fad49fe60",
+    "url": "/bug-busters/three/examples/js/shaders/CopyShader.js"
+  },
+  {
+    "revision": "190b1925bb75ca75e22f093607c6d0bd",
+    "url": "/bug-busters/three/examples/js/shaders/SSAOShader.js"
+  },
+  {
+    "revision": "68d043109303e29e022a55499a9c208d",
+    "url": "/bug-busters/three/examples/js/loaders/3MFLoader.js"
+  },
+  {
+    "revision": "5915fb0d91c92dfc6d47f78ef36a0cd9",
+    "url": "/bug-busters/three/examples/js/loaders/AMFLoader.js"
+  },
+  {
+    "revision": "e64ee057dad825614d50b6389c35afe5",
+    "url": "/bug-busters/three/examples/js/loaders/BVHLoader.js"
+  },
+  {
+    "revision": "5691577d64bd8f0157bdc6592efe7bba",
+    "url": "/bug-busters/three/examples/js/loaders/MD2Loader.js"
+  },
+  {
+    "revision": "222e07424876fb0ba6f54480a93984ed",
+    "url": "/bug-busters/three/examples/js/loaders/MTLLoader.js"
+  },
+  {
+    "revision": "340c1627c4a631ab99215080fa944b33",
+    "url": "/bug-busters/three/examples/js/loaders/NRRDLoader.js"
+  },
+  {
+    "revision": "b6aa8f444e2f426c43834b6a4d86b5c6",
+    "url": "/bug-busters/three/examples/js/loaders/PLYLoader.js"
+  },
+  {
+    "revision": "45d34e5313110567b6d84ffd4be889a8",
+    "url": "/bug-busters/three/examples/js/loaders/RGBELoader.js"
+  },
+  {
+    "revision": "90156d1d4120419ed26e6dd8b43edee6",
+    "url": "/bug-busters/three/examples/js/loaders/STLLoader.js"
+  },
+  {
+    "revision": "bbba7893ca04745ae7a93a120b148168",
+    "url": "/bug-busters/three/examples/js/loaders/TGALoader.js"
+  },
+  {
+    "revision": "1c65202e5a51fd588aac7c391a158ebb",
+    "url": "/bug-busters/three/examples/js/postprocessing/SSAOPass.js"
+  },
+  {
+    "revision": "03859bca4fec065f702952dc43e54db5",
+    "url": "/bug-busters/three/examples/js/controls/OrbitControls.js"
+  },
+  {
+    "revision": "2dd1df1253ebd5fdc82f5795123894d4",
+    "url": "/bug-busters/three/examples/js/loaders/AWDLoader.js"
+  },
+  {
+    "revision": "f4ffaa4999bceacdcfffa8930b3a5b38",
+    "url": "/bug-busters/three/examples/js/loaders/DRACOLoader.js"
+  },
+  {
+    "revision": "de13c9179478b58ea6fe068281901f5e",
+    "url": "/bug-busters/three/examples/js/loaders/OBJLoader.js"
+  },
+  {
+    "revision": "a04ca3771612cfc348a6ed2284a03f45",
+    "url": "/bug-busters/three/examples/js/postprocessing/OutlinePass.js"
+  },
+  {
+    "revision": "e2e55982ea81de80dacefa5f49bcd41a",
+    "url": "/bug-busters/three/examples/js/loaders/EXRLoader.js"
+  },
+  {
+    "revision": "f9474f9bf151909e6f47034b0aaf78f6",
+    "url": "/bug-busters/three/examples/js/loaders/LDrawLoader.js"
+  },
+  {
+    "revision": "d5e0af40dec1c0585ca70d24090f0069",
+    "url": "/bug-busters/three/examples/js/loaders/SVGLoader.js"
+  },
+  {
+    "revision": "49f71882b6da5f242facc1e03f445eda",
+    "url": "/bug-busters/three/examples/js/loaders/TDSLoader.js"
+  },
+  {
+    "revision": "b34e6d05fef9e4bdaf6659b2b0024dbe",
+    "url": "/bug-busters/three/examples/js/loaders/VRMLLoader.js"
+  },
+  {
+    "revision": "14d34c3ce680e92a786d5ef0fb61ed6c",
+    "url": "/bug-busters/three/examples/js/loaders/VTKLoader.js"
+  },
+  {
+    "revision": "2047bcc27a73242ef6101ae87fa39008",
+    "url": "/bug-busters/three/examples/js/loaders/OBJLoader2.js"
+  },
+  {
+    "revision": "cb2fbf416abf872abeeeb311aae6bf4f",
+    "url": "/bug-busters/three/examples/js/loaders/XLoader.js"
+  },
+  {
+    "revision": "b0b627e4ac80ea9377eb45d757d309d8",
+    "url": "/bug-busters/three/examples/js/loaders/AssimpLoader.js"
+  },
+  {
+    "revision": "40a58984435c33d987d5a63fcc59e2ec",
+    "url": "/bug-busters/three/examples/js/loaders/LoaderSupport.js"
+  },
+  {
+    "revision": "7763ebc0ab24695240bb650d722968a6",
+    "url": "/bug-busters/three/examples/js/loaders/MMDLoader.js"
+  },
+  {
+    "revision": "b94374b047f9277b4cd9faa5df1a69df",
+    "url": "/bug-busters/three/examples/js/shaders/FXAAShader.js"
+  },
+  {
+    "revision": "5699f48dc2f3d753e558116d1c676df4",
+    "url": "/bug-busters/img/chair.jpg"
+  },
+  {
+    "revision": "3c11239fd29bab5512eecd3b5b7a875e",
+    "url": "/bug-busters/three/examples/js/loaders/ColladaLoader.js"
+  },
+  {
+    "revision": "f1b03d91b0484723bb4afadb21a61f19",
+    "url": "/bug-busters/three/examples/js/loaders/GLTFLoader.js"
+  },
+  {
+    "revision": "645cb5c36d18718a1f391720c5f1ed0c",
+    "url": "/bug-busters/three/examples/js/loaders/FBXLoader.js"
+  },
+  {
+    "revision": "43786ebd100f9f47c0aa6d40368b5fd3",
+    "url": "/bug-busters/img/plan.jpg"
+  },
+  {
+    "revision": "e3380cd6486cdc28e34a864c2e28a451",
+    "url": "/bug-busters/img/IB.png"
+  },
+  {
+    "revision": "1fea04f00c36dbf49bba0e2c378b1387",
+    "url": "/bug-busters/three/build/three.min.js"
+  },
+  {
+    "revision": "81c88f06ec97d93d15378db8505dfba2",
+    "url": "/bug-busters/models/saal/Saal8.gltf"
+  },
+  {
+    "revision": "07d2d5eef24627a5d73b150e7992c4f0",
+    "url": "/bug-busters/models/saal/Saal8.bin"
+  }
+];
